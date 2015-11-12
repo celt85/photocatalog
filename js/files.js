fd44ec5c9c3cfa62f684557924e7906cf4d8bc63
mbalda <mbalda@gmail.com>
2015-11-13 00:56:16 +0100

    Working on loading pictures from disc.

---
+++ js/files.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    WinJS.Namespace.define("Application", {
+
+        loadPictureFromDisc: function () {
+            var filePicker = Windows.Storage.Pickers.FileOpenPicker();
+            var photoCOntainer = document.querySelector('#photoContainer');
+
+            filePicker.viewMode = Windows.Storage.Pickers.PickerViewMode.thumbnail;
+            filePicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.picturesLibrary;
+            filePicker.fileTypeFilter.replaceAll([".jpg", ".jpeg"]);
+
+            filePicker.pickSingleFileAsync().then(function (file) {
+                if (file) {
+                    var imageUrl = URL.createObjectURL(file, { oneTimeOnly: true });
+                    photoContainer.src = imageUrl;
+                } else {
+                    photoContainer.innerHTML = "Sorry, file not loaded";
+                }
+            });
+        },
+
+    });
+})();
--- pages/photobrowser/photobrowser.js
+++ pages/photobrowser/photobrowser.js
@@ -3,11 +3,14 @@
 (function () {
     "use strict";
 
-    WinJS.UI.Pages.define("/pages/browser/page.html", {
+    WinJS.UI.Pages.define("/pages/photobrowser/photobrowser.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
             // TODO: Initialize the page here.
+
+            //document.querySelector('#addPhotoCmd').addEventListener('click', this.loadPictureFromDisc, false);
+
         },
 
         unload: function () {
@@ -18,6 +21,8 @@
             /// <param name="element" domElement="true" />
 
             // TODO: Respond to changes in layout.
-        }
+        },
+
+        
     });
 })();
