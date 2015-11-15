db3ad38c2d0f910d7d07eecfc038471a30fde6f5
mbalda <mbalda@gmail.com>
2015-11-15 15:16:48 +0100

    Add  Hub control and Hero image.

--- bin/Debug/AppX/pages/camera/camera.js
+++ bin/Debug/AppX/pages/camera/camera.js
@@ -1,25 +1,53 @@
 ﻿// For an introduction to the Page Control template, see the following documentation:
 // http://go.microsoft.com/fwlink/?LinkId=232511
 (function () {
-    "use strict";
+	"use strict";
 
-    WinJS.UI.Pages.define("/pages/camera/camera.html", {
-        // This function is called whenever a user navigates to this page. It
-        // populates the page elements with the app's data.
-        ready: function (element, options) {
-            // TODO: Initialize the page here.
+	var _photoContainer;
+	var that;
 
-           
-        },
+	WinJS.UI.Pages.define("/pages/camera/camera.html", {
+		// This function is called whenever a user navigates to this page. It
+		// populates the page elements with the app's data.
+		ready: function (element, options) {
+			// TODO: Initialize the page here.
+			_photoContainer = element.querySelector("#photoContainer");
 
-        unload: function () {
-            // TODO: Respond to navigations away from this page.
-        },
+			element.querySelector("#takePicture").onclick = this._takePicture;
 
-        updateLayout: function (element) {
-            /// <param name="element" domElement="true" />
+			that = this;
+		},
 
-            // TODO: Respond to changes in layout.
-        }
-    });
+		unload: function () {
+			// TODO: Respond to navigations away from this page.
+		},
+
+		updateLayout: function (element) {
+			/// <param name="element" domElement="true" />
+
+			// TODO: Respond to changes in layout.
+		},
+
+		_savePicture: function (fileToSave) {
+			var folder = Windows.Storage.KnownFolders.picturesLibrary;
+
+			folder.createFileAsync("photo.jpg", Windows.Storage.CreationCollisionOption.generateUniqueName)
+				.then(function (file) {
+					fileToSave.copyAndReplaceAsync(file);
+				});
+		},
+
+		_takePicture: function () {
+			var camera = new Windows.Media.Capture.CameraCaptureUI();
+
+			var p = camera.captureFileAsync(Windows.Media.Capture.CameraCaptureUIMode.photo);
+			p.done(function (capturedItem) {
+				if (capturedItem) {
+					var imageUrl = URL.createObjectURL(capturedItem, { oneTimeOnly: true });
+					//_photoContainer.src = imageUrl;
+					that._savePicture(capturedItem);
+				}
+			});
+		}
+	});
 })();
