17f946f9d6c627a9ee33c6d4508de37bd6c13be5
mbalda <mbalda@gmail.com>
2015-11-16 16:08:23 +0100

    Fix problems with binding data and events to ListView.

--- pages/photobrowser/photobrowser.js
+++ pages/photobrowser/photobrowser.js
@@ -1,15 +1,19 @@
 ﻿(function () {
     "use strict";
 
-    var viewData;
-
     WinJS.UI.Pages.define("/pages/photobrowser/photobrowser.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
             // TODO: Initialize the page here.
+            this._populateData();
 
-        	document.querySelector('#addPhotoCmd').addEventListener('click', function() {Application.loadPicture(document.querySelector('#photoContainer'))}, false);
+           // var listView = element.querySelector("#picturesListView");
+
+            var that = this;
+            document.querySelector('#addPhotoCmd').addEventListener('click', function () { Application.loadPicture(document.querySelector('#photoContainer')) }, false);
+            document.querySelector('#picturesListView').addEventListener('iteminvoked', that._itemSelected, false);
+           // listView.addEventListener('iteminvoked', itemSelected, false);
         },
 
         unload: function () {
@@ -28,20 +32,22 @@
 
             dir.getFilesAsync().then(function (files) {
                 files.forEach(function (file) {
-                    var item = { fileName: file.name, picture: file.path };
+
+                    var pictureUrl = window.URL.createObjectURL(file, { oneTimeOnly: true });
+                    var item = { fileName: file.name, picture: pictureUrl };
                     data.push(item);
                 });
 
-                viewData = new WinJS.Binding.List(data);
+                document.querySelector('#picturesListView').winControl.itemDataSource = new WinJS.Binding.List(data).dataSource;
             });
         },
 
-        viewData: {
-            get: function () {
-                this._populateData();
+        _itemSelected: function (e) {
+            e.detail.itemPromise.then(function (item) {
+                var img = document.querySelector('#heroPictureContainer');
+                img.src = item.data.picture;
+            });
+        },
 
-                return viewData;
-            }
-        }
     });
 })();
